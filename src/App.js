df4c43d5dc780d449281c5a3c77593d194a8697b
Dheeraj Saini <dheeraj@192.168.1.80>
2025-03-23 01:25:20 +0530

    go

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import React, { useState } from "react";
 import CurrentLocation from "./currentLocation";
 import "./App.css";
 
+
 function App() {
   return (
     <React.Fragment>
       <div className="container">
         <CurrentLocation />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"  />
       </div>
       <div className="footer-info">
-        <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93">
-          Download Source Code
+        <a href="https://github.com/DheerajSaini0001/Weather">
+          Source Code
         </a>{" "}
         | Developed by{" "}
-        <a target="_blank" href="https://www.gauravghai.dev/">
+        <a target="_blank" href="https://www.linkedin.com/in/dheeraj-saini-6256902a9/">
           Dheeraj Saini
         </a>{" "}
-        | Powered by{" "}
-        <a target="_blank" href="https://www.htmlhints.com/">
-          HTML HINTS
-        </a>
+       <br/>
+       <br/>
+        <div className="socialm">
+    <a href="https://www.facebook.com/saini.dheeraj.33" class="Socialfb "><i class="fa-brands fa-facebook-f"></i></a>
+    <a href="https://www.linkedin.com/in/dheeraj-saini-6256902a9?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" class="Sociallinkedin "><i class="fa-brands fa-linkedin"></i></a>
+    <a href="https://www.instagram.com/dheerajsaini069?igsh=MW05YmgzazdrZXU3MA==" class="Socialinstagram "><i class="fa-brands fa-instagram"></i></a>
+    <a href=" https://x.com/Dheerajsaini069?t=TwOL0GS-cDe_fiaAdpMCCA&s=08 " class="Socialtwitter "><i class="fa-brands fa-twitter"></i></a>
+    </div>
       </div>
     </React.Fragment>
   );
