a43144b9648715845c794d40f3f859aec8687963
Dheeraj Saini <dheeraj@192.168.1.80>
2025-03-23 00:32:01 +0530

    go

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "98dbc068734c8ee39f1be47b256ae195",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
