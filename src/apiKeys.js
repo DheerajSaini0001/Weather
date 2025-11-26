9b12bcdb93540d9eedd3b1390565cb789641aa02
Dheeraj Saini <dheerajsaini131652@gmail.com>
2025-11-26 15:18:25 +0530

    DOne for the env file

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "98dbc068734c8ee39f1be47b256ae195",
+  key: process.env.REACT_APP_WEATHERKEY || "98dbc068734c8ee39f1be47b256ae195",
   base: "https://api.openweathermap.org/data/2.5/",
 };
