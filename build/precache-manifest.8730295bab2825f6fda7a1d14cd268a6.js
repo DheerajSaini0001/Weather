9cbe7baa6caaa92ad72580b123fb3539c6f42ebb
Dheeraj Saini <dheerajsaini131652@gmail.com>
2025-11-28 11:21:06 +0530

    Fix build for Render and add render.yaml

---
+++ build/precache-manifest.8730295bab2825f6fda7a1d14cd268a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc74f003ca3fb73ac8fa6a9352975624",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05087a2a57f25519449a",
+    "url": "/static/css/main.10751368.chunk.css"
+  },
+  {
+    "revision": "373b291555817d953b75",
+    "url": "/static/js/2.a0289d9b.chunk.js"
+  },
+  {
+    "revision": "56d95550feb01ef0b6ca1d51614c975e",
+    "url": "/static/js/2.a0289d9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05087a2a57f25519449a",
+    "url": "/static/js/main.cebb1f16.chunk.js"
+  },
+  {
+    "revision": "1d34c1ef83c210c405ce",
+    "url": "/static/js/runtime-main.e9946a1e.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/static/media/WeatherIcons.0a218296.gif"
+  }
+]);
